59c280e7165f59a790365103c7026e1c6993cff6
44David <david.soane@outlook.com>
2024-12-25 10:26:38 -0600

    Fixed error where matplotlib would not appear

--- visualisation.py
+++ visualisation.py
@@ -23,6 +23,7 @@ def display_image(image):
     fig = plt.figure(figsize=(20, 15))
     plt.grid(False)
     plt.imshow(image)
+    plt.show()
 
 def download_resize_img(url, n_width=256, n_height=256, display=False):
     _, filename = tempfile.mkstemp(suffix=".jpg")
@@ -93,4 +94,44 @@ def draw_boxes(image, boxes, class_names, scores, max_boxes=10, min_score=0.1):
             image_pil = Image.fromarray(np.uint8(image)).convert("RGB")
 
             draw_bounding_box(image_pil, ymin, xmin, ymax, xmax, color, font, display_str_list=[display_str])
+            np.copyto(image, np.array(image_pil))
     return image
+
+
+
+image_url = "https://upload.wikimedia.org/wikipedia/commons/6/60/Naxos_Taverna.jpg" 
+downloaded_image_path = download_resize_img(image_url, 1280, 856, True)
+
+module_handle = "https://tfhub.dev/google/faster_rcnn/openimages_v4/inception_resnet_v2/1"
+
+detector = hub.load(module_handle).signatures['default']
+
+def load_img(path):
+    img = tf.io.read_file(path)
+    img = tf.image.decode_jpeg(img, channels=3)
+
+    return img
+
+
+def run_detector(detector, path):
+    img = load_img(path)
+
+    converted_img = tf.image.convert_image_dtype(img, tf.float32)[tf.newaxis, ...]
+    start_time = time.time()
+    result = detector(converted_img)
+    end_time = time.time()
+
+    result = {key:value.numpy() for key, value in result.items()}
+
+    print("Found %d objects." % len(result["detection_scores"]))
+    print("Inference time: ", end_time-start_time)
+
+    image_with_boxes = draw_boxes(
+        img.numpy(), result["detection_boxes"],
+        result["detection_class_entities"], result["detection_scores"]
+    )
+
+    display_image(image_with_boxes)
+
+
+run_detector(detector, downloaded_image_path)
